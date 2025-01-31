da742ca052a18c5be9815fb8ab7c0107b458c83f
Itamar Simão <102179953+ItaSimao@users.noreply.github.com>
2025-01-31 07:15:15 -0300

    Criando os microservices User e Email com Spring Boot Implementando o endpoint save users

--- user/src/main/java/com/ms/user/services/UserService.java
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -11,7 +11,6 @@ public class UserService {
     final UserRepository userRepository;
 
 
-
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
