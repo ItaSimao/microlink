c08d175ab760944794616a1a3254c3807f75d79e
Itamar Simão <102179953+ItaSimao@users.noreply.github.com>
2025-02-06 06:49:10 -0300

    LISTEN MESSAGE

--- email/src/test/java/com/ms/email/EmailConsumers.java
+++ email/src/test/java/com/ms/email/EmailConsumers.java
@@ -1,5 +1,6 @@
 package com.ms.email;
 
+import com.ms.email.dtos.EmailRecordDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -7,9 +8,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailConsumers {
 
-    @RabbitListener(queues = "${broker.queue.email.name}" )
-    public void listenEmailQueue(@Payload String string) {
-        System.out.println(string);
+    @RabbitListener(queues = "default.email" )
+    public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto) {
+        System.out.println(emailRecordDto.emailTo());
     }
 
 }
---
+++ email/src/test/java/com/ms/email/dtos/EmailRecordDto.java
@@ -0,0 +1,10 @@
+package com.ms.email.dtos;
+
+import java.util.UUID;
+
+public record EmailRecordDto(UUID UserId,
+                             String emailTo,
+                             String subject,
+                             String text) {
+
+}
