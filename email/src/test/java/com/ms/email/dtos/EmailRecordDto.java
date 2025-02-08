7e219f0164c1645c17bf84258f842289dbdff230
Itamar Simão <102179953+ItaSimao@users.noreply.github.com>
2025-02-08 06:06:14 -0300

    LISTEN MESSAGE

--- email/src/test/java/com/ms/email/dtos/EmailRecordDto.java
+++ email/src/test/java/com/ms/email/dtos/EmailRecordDto.java
@@ -2,6 +2,7 @@ package com.ms.email.dtos;
 
 import java.util.UUID;
 
+
 public record EmailRecordDto(UUID UserId,
                              String emailTo,
                              String subject,
