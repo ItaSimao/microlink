11cb886568eea73e282633022208cb7d21b11a4e
Itamar Simão <102179953+ItaSimao@users.noreply.github.com>
2025-02-11 06:04:01 -0300

    CORRIGINDO BUGS

--- email/src/main/java/com/ms/email/EmailApplication.java
+++ email/src/main/java/com/ms/email/EmailApplication.java
@@ -6,11 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmailApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(EmailApplication.class, args);
 	}
-
-
 
 }
